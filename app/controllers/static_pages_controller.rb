31120e0bdd9e055c28d7928e1f8b7b9ce50d0018
Adam Niedzielski <adamsunday@gmail.com>
2016-03-21 14:26:00 +0100

    Add stubs of static pages. Closes #2.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def impressum
+  end
+
+  def privacy_policy
+  end
+
+  def terms
+  end
+
+  def submit_your_story
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  root to: "static_pages#index"
+
+  get "impressum" => "static_pages#impressum"
+  get "privacy_policy" => "static_pages#privacy_policy"
+  get "terms" => "static_pages#terms"
+  get "submit_your_story" => "static_pages#submit_your_story"
 end
