6ff0649568c471e4f4ddc8ff377e586d9efc4c6d
Adam Niedzielski <adamsunday@gmail.com>
2016-03-29 09:33:52 +0200

    Add exception handling when payment fails.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,18 @@
 class PaymentsController < ApplicationController
   def pay
-    stripe_token = params.fetch(:stripeToken)
     DI.get(PayForCart).call(current_visitor, stripe_token)
-    redirect_to root_url, notice: "Thank you for paying!"
+    redirect_to root_url, notice: t("payment.success")
+  rescue MakeStripePayment::PaymentFailedDueToCustomer => ex
+    redirect_to cart_url, flash: {
+      error: t("payment.failure.customer_error", reason: ex.message)
+    }
+  rescue MakeStripePayment::PaymentFailedDueToUs
+    redirect_to cart_url, flash: { error: t("payment.failure.server_error") }
+  end
+
+  private
+
+  def stripe_token
+    params.fetch(:stripeToken)
   end
 end
--- app/services/make_stripe_payment.rb
+++ app/services/make_stripe_payment.rb
@@ -1,19 +1,28 @@
 class MakeStripePayment
-  class PaymentFailed < RuntimeError; end
+  class PaymentFailedDueToCustomer < RuntimeError; end
+  class PaymentFailedDueToUs < RuntimeError; end
 
   def self.build
     new
   end
 
   def call(cart, token)
+    create_charge(cart.total, token)
+  rescue Stripe::CardError => ex
+    Rollbar.warning(ex)
+    raise PaymentFailedDueToCustomer, ex.message
+  rescue Stripe::StripeError => ex
+    Rollbar.error(ex)
+    raise PaymentFailedDueToUs
+  end
+
+  private
+
+  def create_charge(total, token)
     Stripe::Charge.create(
-      amount: cart.total.cents,
-      currency: cart.total.currency,
+      amount: total.cents,
+      currency: total.currency,
       source: token
     )
-  rescue Stripe::CardError
-    raise PaymentFailed
-  rescue Stripe::StripeError
-    raise PaymentFailed
   end
 end
