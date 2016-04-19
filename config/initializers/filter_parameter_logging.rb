b398b53363ab9f474462f7cf4045aba752f7d39b
Adam Niedzielski <adamsunday@gmail.com>
2016-04-19 14:00:26 +0200

    Filter out long values from request parameters in the logs.
    Prepare for Base64 encoded files sent to the API.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,3 +2,8 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters << lambda do |_, value|
+  if value.class == String && value.length > 1024
+    value.replace(value.truncate(1024))
+  end
+end
