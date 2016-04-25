0b87efadde305e7f8a198a40f77f93c8c9985f0a
Adam Niedzielski <adamsunday@gmail.com>
2016-04-25 10:23:46 +0200

    Change hash keys to strings in EventPublisher.
    Hash indexed by classes caused problems with Rails code reloading.

--- lib/event_publisher.rb
+++ lib/event_publisher.rb
@@ -2,16 +2,16 @@ class EventPublisher
   @handlers = Hash.new { |hash, key| hash[key] = [] }
 
   def self.publish(event)
-    @handlers[event.class].each do |handler|
+    @handlers[event.class.name].each do |handler|
       handler.call(event)
     end
   end
 
   def self.push_handler(event_class, handler)
-    @handlers[event_class] << handler
+    @handlers[event_class.name] << handler
   end
 
   def self.clear_handlers(event_class)
-    @handlers[event_class] = []
+    @handlers[event_class.name] = []
   end
 end
