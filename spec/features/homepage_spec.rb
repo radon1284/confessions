9da1e0171cc0b1bbf799671d686011971efe7d6b
Adam Niedzielski <adamsunday@gmail.com>
2016-06-24 09:04:44 +0200

    Change spec for homepage to reflect the new content.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,12 +1,8 @@
 require "rails_helper"
 
 describe "Homepage" do
-  let!(:book) do
-    FactoryGirl.create(:book, slug: "awesome-book", title: "Awesome book")
-  end
-
-  it "displays the list of books" do
+  it "displays the personal information" do
     visit root_path
-    expect(page).to have_content("Awesome book")
+    expect(page).to have_content("I call myself an analyst")
   end
 end
