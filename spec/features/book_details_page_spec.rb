5a66a6cd4fffb92ff81c57f01f2c174166a09c34
Adam Niedzielski <adamsunday@gmail.com>
2016-07-01 13:56:18 +0200

    Display chapter title when linking to it.

--- spec/features/book_details_page_spec.rb
+++ spec/features/book_details_page_spec.rb
@@ -23,14 +23,14 @@ describe "Book details page" do
 
   describe "previews" do
     before do
-      FactoryGirl.create(:chapter, book: book, number: 1)
-      FactoryGirl.create(:chapter, book: book, number: 2)
+      FactoryGirl.create(:chapter, book: book, number: 1, title: "First")
+      FactoryGirl.create(:chapter, book: book, number: 2, title: "Second")
     end
 
     it "displays chapters" do
       visit book_path("cto")
-      expect(page).to have_content("Chapter 1")
-      expect(page).to have_content("Chapter 2")
+      expect(page).to have_content("Chapter 1 - First")
+      expect(page).to have_content("Chapter 2 - Second")
     end
   end
 end
--- spec/features/chapter_preview_page_spec.rb
+++ spec/features/chapter_preview_page_spec.rb
@@ -1,16 +1,29 @@
 require "rails_helper"
 
 describe "Chapter preview page" do
+  let!(:book) { FactoryGirl.create(:book) }
   let!(:chapter) do
     FactoryGirl.create(
       :chapter,
-      content_html: "<h1>Best chapter</h1>"
+      content_html: "<h1>Best chapter</h1>",
+      book: book
     )
   end
 
   it "shows chapter content" do
-    visit book_chapter_path(chapter.book.slug, chapter.number)
+    visit book_chapter_path(book.slug, chapter.number)
     expect(page).to have_content("Best chapter")
     expect(page).to have_no_content("<h1>")
+  end
+
+  describe "navigation" do
+    let!(:other_chapter) do
+      FactoryGirl.create(:chapter, title: "Other chapter", number: 2, book: book)
+    end
+
+    it "links to other chapters in this book" do
+      visit book_chapter_path(book.slug, chapter.number)
+      expect(page).to have_content("Chapter 2 - Other chapter")
+    end
   end
 end
