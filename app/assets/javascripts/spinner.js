72736e1f44b88cc39adfc1d153c6b035de336f31
Jack Kinsella <jack.kinsella@gmail.com>
2016-07-30 00:42:09 +0200

    spinner on load

---
+++ app/assets/javascripts/spinner.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  preloaderFadeOutTime = 500;
+  maxLoaderTime = 1500;
+
+  function hidePreloader() {
+    var preloader = $('.spinner-wrapper');
+    preloader.fadeOut(preloaderFadeOutTime);
+  }
+
+  $(window).on('load', (function() {
+    hidePreloader();
+  }));
+
+  setTimeout(hidePreloader, maxLoaderTime)
+});
