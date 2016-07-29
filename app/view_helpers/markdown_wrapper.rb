cb7e7dec4ef395a4066048d666e080273253359e
Jack Kinsella <jack.kinsella@gmail.com>
2016-07-29 14:39:25 +0200

    switch to Github Flavour Markdown

--- app/view_helpers/markdown_wrapper.rb
+++ app/view_helpers/markdown_wrapper.rb
@@ -1,5 +1,12 @@
 class MarkdownWrapper
   def display(markdown)
-    Kramdown::Document.new(markdown).to_html.html_safe
+    Kramdown::Document.new(markdown, kramdown_options).to_html.html_safe
+  end
+
+  private
+
+  def kramdown_options
+    {syntax_highlighter: nil,
+     input: "GFM"}
   end
 end
