6993ef264488ed8cef0b0dd201f0410a8ccfc34c
Adam Niedzielski <adamsunday@gmail.com>
2016-08-04 11:39:59 +0200

    Fix test after copy change.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 describe "Homepage" do
   it "displays the personal information" do
     visit root_path
-    expect(page).to have_content("I call myself an analyst")
+    expect(page).to have_content("Jack Kinsella (Over-)Analyst")
   end
 end
