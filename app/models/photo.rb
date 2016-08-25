a198ad4ecd9dab6daabfdaea74817c31b8b33dc3
Jack Kinsella <jack.kinsella@gmail.com>
2016-08-25 19:54:34 +0200

    ensure photo has a unique filename to prevent potential clashes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,15 @@
 class Photo < ActiveRecord::Base
   mount_uploader :file, PhotoUploader
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) ||
+      model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
