7ddebacfd0dc3b7bd4e8de09142219765ab1c93e
Adam Niedzielski <adamsunday@gmail.com>
2016-09-10 16:19:40 +0200

    Test GenerateFinancialTransactionsReport::FetchBalanceTransactions.
    Closes #105.

---
+++ spec/services/generate_financial_transactions_report/fetch_from_stripe_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+# We cannot test too much stuff here, because the test depends on balance
+# transactions which already are in our Stripe account. This means that
+# we do not have control over the test environment.
+
+describe GenerateFinancialTransactionsReport::FetchFromStripe, :vcr do
+  let!(:service) do
+    GenerateFinancialTransactionsReport::FetchFromStripe.build
+  end
+
+  it "returns all balance transactions in the given period" do
+    result = service.call(
+      DateTime.new(2016, 9, 1),
+      DateTime.new(2016, 9, 30).end_of_day
+    )
+    expect(result).to be_present
+  end
+end
