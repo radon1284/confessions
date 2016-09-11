59bcfb00665dda3b64ab0efc695cae45fd236c2e
Adam Niedzielski <adamsunday@gmail.com>
2016-09-11 16:37:32 +0200

    Do not allow outgoing HTTP connections when VCR is disabled.
    I am actually quite surprised why I added this line 5 months ago.
    This is not really a good idea, because we want to precisely control
    all the requests in tests.
    "ignore_localhost" solves the problem with Capybara and JavaScript driver.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.allow_http_connections_when_no_cassette = true
+  c.ignore_localhost = true
   c.filter_sensitive_data('<STRIPE_TOKEN>') do
     ENV.fetch("STRIPE_SECRET_KEY")
   end
