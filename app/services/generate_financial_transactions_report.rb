4ddfefd68bb5c22a9bcc6a8412b57e40bd7b466f
Adam Niedzielski <adamsunday@gmail.com>
2016-09-12 11:49:05 +0200

    Implement a service object for calculating a purchase code in financial transactions report.

--- app/services/generate_financial_transactions_report.rb
+++ app/services/generate_financial_transactions_report.rb
@@ -1,4 +1,10 @@
 class GenerateFinancialTransactionsReport
+  STRIPE_FEE_CODE = 4971
+  WITHDRAWAL_CODE = 1360
+  GERMANY_PURCHASE_CODE = 8400
+  EU_PURCHASE_CODE = 8331
+  OUTSIDE_EU_PURCHASE_CODE = 8338
+
   def self.build
     new
   end
