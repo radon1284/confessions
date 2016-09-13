892e9e0d8400f8c477208fe19dd1dc13cf9c2f6e
Adam Niedzielski <adamsunday@gmail.com>
2016-09-13 10:45:07 +0200

    Enable Rails specific Rubocop cops. Closes #107.

--- app/view_helpers/markdown_wrapper.rb
+++ app/view_helpers/markdown_wrapper.rb
@@ -1,7 +1,9 @@
 class MarkdownWrapper
+  # rubocop:disable Rails/OutputSafety
   def display(markdown)
     Kramdown::Document.new(markdown, kramdown_options).to_html.html_safe
   end
+  # rubocop:enable Rails/OutputSafety
 
   private
 
